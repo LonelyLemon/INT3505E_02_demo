99d8063730a17ae05d381482060f0d1153cb8b15
LonelyLemon <rulu498@gmail.com>
2025-11-27 15:46:38 +0000

    Add swagger.yaml for both week10 week11

--- Week11/app/__init__.py
+++ Week11/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.extensions import db, jwt, limiter, metrics
 from app.auth.routes import auth_bp
 from app.books.routes import books_bp
+from flasgger import Swagger
 import logging
 import sys
 
@@ -10,20 +11,25 @@ def create_app():
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JWT_SECRET_KEY'] = '482e91425bd0a7ad5e00d94544cbc345'
+    app.config['JWT_SECRET_KEY'] = 'your-super-secret-key'
+
+    app.config['SWAGGER'] = {
+        'title': 'Library API Documentation',
+        'uiversion': 2
+    }
 
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
-        handlers=[
-            logging.StreamHandler(sys.stdout)
-        ]
+        handlers=[logging.StreamHandler(sys.stdout)]
     )
 
     db.init_app(app)
     jwt.init_app(app)
     limiter.init_app(app)
     metrics.init_app(app)
+    
+    Swagger(app, template_file='../swagger.yaml')
 
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(books_bp, url_prefix='/books')
